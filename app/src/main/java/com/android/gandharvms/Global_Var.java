4d65e8eefbde3115b8c13da29a4d1f5ffb3da826
Prince Gupta <guptaprince27012001@gmail.com>
2025-06-11 16:42:43 +0530

    Updated The App Version to 1.0.13

--- app/src/main/java/com/android/gandharvms/Global_Var.java
+++ app/src/main/java/com/android/gandharvms/Global_Var.java
@@ -16,7 +16,7 @@ public class Global_Var {
     public String ProposeProcess;
     public String Token;
     public int InwardId;
-    public String APKversion="1.0.12";
+    public String APKversion="1.0.13";
     // Getter/setter
     private static Global_Var instance;
     public static Global_Var getInstance() {
